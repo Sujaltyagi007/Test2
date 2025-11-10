206acaa6956238bc0eaf878f85ba50ddf757fd02
test user <testuser@gmail.com>
2025-11-10 16:08:02 +0530

    something

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "export", // For static export
+  basePath: "/Test2",
+  assetPrefix: "/Test2/",
 };
 
 export default nextConfig;
